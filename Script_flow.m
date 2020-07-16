cae96a01a42f78062e2ab73d09421310046b6d7d
seyfeddineboukhtache <33892951+seyfeddineboukhtache@users.noreply.github.com>
2020-07-16 12:53:00 +0200

    Update Script_flow.m

--- Script_flow.m
+++ Script_flow.m
@@ -3,7 +3,7 @@ clear all
 close all
 
 % If you do not use the star frames: 
-    % 1) Set the good names of the csv files  *_disp_x.csv and *_disp_y.csv
+    % 1) Set the good names of the displacements files  *_disp_x.csv and *_disp_y.csv
     % 2) Change or comment the command caxis([-0.5 0.5]) 
 
 disp_x = csvread('Star_disp_x.csv'); 
